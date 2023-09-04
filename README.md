99ef9997f0f2203c221c92c92366973f4d61413c
arturSantos <artur.santos@southsystem.com.br>
2023-09-04 09:30:10 -0300

    ajuste projeto + php

---
+++ README.md
@@ -0,0 +1,5 @@
+# Finance Control
+
+A project for an Ent to End application for personal financial control, using (currently in its V1) HTML, CSS, JavaScript, PHP and MySQL database, as these were the languages ​​suggested by the professor of the Special Applications Implementation course.
+
+In the future, after the completion of the chair and general validation, in its V2, the plans are to migrate the backend to Java and the database to PostgreSQL
---
+++ db_script.sql
@@ -0,0 +1,82 @@
+CREATE TABLE IF NOT EXISTS artur_user
+(
+    id       INT AUTO_INCREMENT,
+    name     VARCHAR(75) NOT NULL,
+    email    VARCHAR(50) NOT NULL,
+    password VARCHAR(75) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS artur_financial_institution
+(
+    id      INT AUTO_INCREMENT,
+    user_id INT         NOT NULL,
+    name    VARCHAR(50) NOT NULL,
+    address VARCHAR(150),
+    contact VARCHAR(100),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES artur_user (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS artur_account
+(
+    id                       INT AUTO_INCREMENT,
+    user_id                  INT         NOT NULL,
+    financial_institution_id INT         NOT NULL,
+    name                     VARCHAR(50) NOT NULL,
+    description              VARCHAR(150),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES artur_user (id),
+    FOREIGN KEY (financial_institution_id) REFERENCES artur_financial_institution (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS artur_transaction_locale
+(
+    id      INT AUTO_INCREMENT,
+    user_id INT         NOT NULL,
+    name    VARCHAR(50) NOT NULL,
+    address VARCHAR(150),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES artur_user (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS artur_category
+(
+    id          INT AUTO_INCREMENT,
+    user_id     INT         NOT NULL,
+    name        VARCHAR(50) NOT NULL,
+    description VARCHAR(250),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES artur_user (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS artur_transaction
+(
+    id                    INT AUTO_INCREMENT,
+    user_id               INT          NOT NULL,
+    account_id            INT          NOT NULL,
+    transaction_locale_id INT,
+    category              VARCHAR(100) NOT NULL,
+    value                 DOUBLE       NOT NULL,
+    date                  DATE         NOT NULL,
+    type                  VARCHAR(10)  NOT NULL,
+    plots_number          INT,
+    obs                   VARCHAR(255),
+    is_installments       BOOLEAN DEFAULT FALSE,
+    last_charge           DATE,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES artur_user (id),
+    FOREIGN KEY (account_id) REFERENCES artur_account (id),
+    FOREIGN KEY (transaction_locale_id) REFERENCES artur_transaction_locale (id)
+);
